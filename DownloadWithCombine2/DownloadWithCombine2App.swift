6f4221c9d411aa2d603b358bb861046a5db08cbf
fbara <fbara1@yahoo.com>
2021-12-18 15:55:29 -0600

    Initial Commit

---
+++ DownloadWithCombine2/DownloadWithCombine2App.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadWithCombine2App.swift
+//  DownloadWithCombine2
+//
+//  Created by Frank Bara on 12/18/21.
+//
+
+import SwiftUI
+
+@main
+struct DownloadWithCombine2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
